ed119b8e338c1e6bdbaa6c816a8a9444352bb20d
MartinKotas <kotasmartin1997@gmail.com>
2023-01-28 11:47:48 +0000

    changed

--- mewe/src/App.js
+++ mewe/src/App.js
@@ -1,9 +1,8 @@
-
-import Jumbo from './components/Jumbo';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './stylesheet.css'
-
+import Jumbo from "./components/Jumbo";
+import NavbarTop from "./components/Navbar";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./stylesheet.css";
 
 function App() {
   return (
